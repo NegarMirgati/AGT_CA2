db403ee027748cc39fd1be272127fc056533128d
Negar Mirgati <negar.nbj96@yahoo.com>
2018-04-20 18:12:45 +0430

    some for loops changed to apply

--- CA2_2_RWC.R
+++ CA2_2_RWC.R
@@ -4,11 +4,12 @@ gnet <- read.graph("/Users/Negar/Desktop/AGT_CA2/gnet.graphml", format = c("grap
 num_of_vertices <- gorder(gnet)
 gnet_adj_mat <- as_adjacency_matrix(gnet, attr="weight")
 transition_matrix <- matrix(0, nrow = num_of_vertices, ncol = num_of_vertices)
-rowsum_adj_mat <- matrix(0, nrow = num_of_vertices)
+rowsum_adj_mat <- matrix(0, nrow = num_of_vertices, ncol = 1)
 
-for (row in 1 : nrow(gnet_adj_mat)){
-  rowsum_adj_mat[row] <- sum(gnet_adj_mat[row,])
-}
+#for (row in 1 : nrow(gnet_adj_mat)){
+# rowsum_adj_mat[row] <- sum(gnet_adj_mat[row,])
+#}
+rowsum_adj_mat <- apply(gnet_adj_mat, 1, function(x) sum(x))
 
 for(row in 1 : nrow(transition_matrix)){
   for (column in 1 : ncol(transition_matrix)){
@@ -28,8 +29,9 @@ for (j in 1 : ncol(mean_firs_passage_t)){
 }
 
 RWC <- matrix(0, nrow = 1, ncol = num_of_vertices)
-for(i in 1 : ncol(RWC)){
-  #print(i)
-  RWC[1,i] <- num_of_vertices / sum(mean_firs_passage_t[,i])
-}
+RWC <- apply(mean_firs_passage_t, 2, function(x) num_of_vertices / sum(x))
 
+#for(i in 1 : ncol(RWC)){
+  #print(i)
+ # RWC[1,i] <- num_of_vertices / sum(mean_firs_passage_t[,i])
+#}
